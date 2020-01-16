7a582f9f4a89114be593b024429f1c55bbcfeeba
Luicery <56919859+Luicery@users.noreply.github.com>
2020-01-16 15:43:50 +1100

    Added slideshow to the properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,8 +1,18 @@
 class PropertiesController < ApplicationController
   def new
+    if session[:agent_id].present? == false
+      redirect_to properties_path
+    end
+    @property = Property.new
   end
 
   def create
+    @property = Property.create property_params
+    newImage = Image.create image:params[:images]
+    @property.images << newImage
+    listingAgent = Agent.find @current_user.id
+    listingAgent.properties << @property
+    redirect_to properties_path
   end
 
   def show
@@ -11,9 +21,13 @@ class PropertiesController < ApplicationController
   end
 
   def edit
+    @property = Property.find params[:id]
   end
 
   def update
+    property = Property.find params[:id]
+    property.update property_params
+    redirect_to(properties_path)
   end
 
   def destroy
@@ -21,5 +35,9 @@ class PropertiesController < ApplicationController
 
   def index
     @property = Property.all
+  end
+  private
+  def property_params
+    params.require(:property).permit( :title, :price, :bathrooms, :bedrooms, :parking, :address, :area, :description)
   end
 end
